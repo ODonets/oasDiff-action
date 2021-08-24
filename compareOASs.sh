731aec5d2305d01affedcad18554d3b63e46f5e5
Oleks Donets <donets.oleksii@gmail.com>
2021-08-24 18:05:08 +0200

    test2

--- compareOASs.sh
+++ compareOASs.sh
@@ -1,11 +1,11 @@
 
 #assign descriptiv variables to the values of arguments
-INPUT_FILE_OLD=$1
-INPUT_FILE_NEW=$2
+OAS_FILE_OLD=$1
+OAS_FILE_NEW=$2
 
 # compare two files (old and new) and catch the output and write the result into the file 
 set +e
-docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$INPUT_FILE_OLD /data/$INPUT_FILE_NEW --fail-on-incompatible 
+docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible > changeLog.md
 exit_code=$?
 set -e
 if [[ $exit_code == 0 ]]; then
