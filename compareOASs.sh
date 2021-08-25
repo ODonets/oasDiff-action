ee7dbdae6c3f915b51cb80d42977e72ad245e1e5
Oleks Donets <donets.oleksii@gmail.com>
2021-08-25 09:07:32 +0200

    back to the version of yesterday

--- compareOASs.sh
+++ compareOASs.sh
@@ -1,21 +1,18 @@
-
 #assign descriptiv variables to the values of arguments
-PATH=$1
-OAS_FILE_OLD=$2
-OAS_FILE_NEW=$3
-CHANGE_LOG_FILE=${PATH}/$4
+OAS_FILE_OLD=$1
+OAS_FILE_NEW=$2
 
-# compare two files (old and new) and catch the output and write the result into the file
+# compare two files (old and new) and catch the output and write the result into the file 
 set +e
-docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible
+docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible > changeLog.md
 exit_code=$?
 set -e
 if [[ $exit_code == 0 ]]; then
-  echo "Comparison of two OASs files was successfull"
+  echo "success"
 elif [[ $exit_code == 1 ]]; then
-  echo "::warning:: The new OAS version is not compatible with the previous one."
+  echo "::warning::not compatible"
   exit 1
 else
-  echo "::error::Execution of the comparison failed, comparision script exit_code is $exit_code"
+  echo "::error::execution failed, exit_code=$exit_code"
   exit 2
 fi
