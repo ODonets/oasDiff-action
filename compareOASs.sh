7fc6c66ac31e95606a67ff4845d72e87e0e46584
Oleks Donets <donets.oleksii@gmail.com>
2021-08-25 10:31:04 +0200

    test succsess

--- compareOASs.sh
+++ compareOASs.sh
@@ -2,22 +2,22 @@
 WORKING_DIR=$1
 OAS_FILE_OLD=$2
 OAS_FILE_NEW=$3
-CHANGE_LOG_FILE=${WORKING_DIR}/$4
+CHANGE_LOG_DIR=${WORKING_DIR}/$4
 
 # compare two files (old and new) and catch the output and write the result into the file 
 set +e
-echo "CHANGE_LOG_FILE = $CHANGE_LOG_FILE"
+echo "CHANGE_LOG_DIR = $CHANGE_LOG_DIR"
 output=$(docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible)
 exit_code=$?
 set -e
 if [[ $exit_code == 0 ]]; then
   echo "Comparison of two OASs files was successfull"
-  echo "$output" > "$CHANGE_LOG_FILE"
+  echo "$output" > "$CHANGE_LOG_DIR"
   echo "Exit code of the file writing command is $?"
   exit $?
 elif [[ $exit_code == 1 ]]; then
   echo "::warning:: The new OAS version is not compatible with the previous one."
-  echo "$output" > "$CHANGE_LOG_FILE"
+  echo "$output" > "$CHANGE_LOG_DIR"
   echo "Exit code of the file writing command is  $?"
   exit 1
 else
