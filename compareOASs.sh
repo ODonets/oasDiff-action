86a707fdb6cab5795a59339c589f9ac517b52d1d
Oleks Donets <donets.oleksii@gmail.com>
2021-08-25 08:42:04 +0200

    intro path

--- compareOASs.sh
+++ compareOASs.sh
@@ -1,19 +1,24 @@
 
 #assign descriptiv variables to the values of arguments
-OAS_FILE_OLD=$1
-OAS_FILE_NEW=$2
+PATH=$1
+OAS_FILE_OLD=$2
+OAS_FILE_NEW=$3
+CHANGE_LOG_FILE=${PATH}/$4
 
-# compare two files (old and new) and catch the output and write the result into the file 
+# compare two files (old and new) and catch the output and write the result into the file
 set +e
-docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible > changeLog.md
+echo "OAS_FILE_OLD = $OAS_FILE_OLD"
+echo "OAS_FILE_NEW = $OAS_FILE_NEW"
+echo "CHANGE_LOG_FILE = $CHANGE_LOG_FILE"
+docker run --rm -t -v $(pwd)/$PATH:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible > $CHANGE_LOG_FILE
 exit_code=$?
 set -e
 if [[ $exit_code == 0 ]]; then
-  echo "success"
+  echo "Comparison of two OASs files was successfull"
 elif [[ $exit_code == 1 ]]; then
-  echo "::warning::not compatible"
+  echo "::warning:: The new OAS version is not compatible with the previous one."
   exit 1
 else
-  echo "::error::execution failed, exit_code=$exit_code"
+  echo "::error::Execution of the comparison failed, comparision script exit_code is $exit_code"
   exit 2
 fi
