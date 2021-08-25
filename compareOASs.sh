624a74f0c51b02c458eae3cdfbaae43588478a3e
Oleks Donets <donets.oleksii@gmail.com>
2021-08-25 08:54:47 +0200

    delete to file

--- compareOASs.sh
+++ compareOASs.sh
@@ -10,7 +10,7 @@ set +e
 echo "OAS_FILE_OLD = $OAS_FILE_OLD"
 echo "OAS_FILE_NEW = $OAS_FILE_NEW"
 echo "CHANGE_LOG_FILE = $CHANGE_LOG_FILE"
-docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible > $CHANGE_LOG_FILE
+docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible
 exit_code=$?
 set -e
 if [[ $exit_code == 0 ]]; then
