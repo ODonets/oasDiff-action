254121686e29dc941377b39fb6732e4ad81132e2
Oleks Donets <donets.oleksii@gmail.com>
2021-08-25 10:37:22 +0200

    test upload of differences

--- compareOASs.sh
+++ compareOASs.sh
@@ -7,7 +7,7 @@ CHANGE_LOG_DIR=${WORKING_DIR}/$4
 # compare two files (old and new) and catch the output and write the result into the file 
 set +e
 echo "CHANGE_LOG_DIR = $CHANGE_LOG_DIR"
-output=$(docker run --rm -t -v $(pwd)/${WORKING_DIR}:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible)
+output=$(docker run --rm -t -v $(pwd)/oas:/data openapitools/openapi-diff:latest /data/$OAS_FILE_OLD /data/$OAS_FILE_NEW --fail-on-incompatible)
 exit_code=$?
 set -e
 if [[ $exit_code == 0 ]]; then
